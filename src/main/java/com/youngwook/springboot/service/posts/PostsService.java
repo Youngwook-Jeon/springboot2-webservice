4a0e3f3b3194f85082e5a05158be6a2a5fb08942
Young wook Jeon <ela87@naver.com>
2020-02-25 20:19:46 +0900

    등록 API 추가

---
+++ src/main/java/com/youngwook/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.youngwook.springboot.service.posts;
+
+import com.youngwook.springboot.domain.posts.PostsRepository;
+import com.youngwook.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
