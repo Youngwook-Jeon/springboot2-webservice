f7aecfde4c7643753975433a91c563f36fd1b4bb
Young wook Jeon <ela87@naver.com>
2020-02-26 20:03:37 +0900

    수정/조회 API 추가, 메인 페이지 뷰 추가

--- src/main/java/com/youngwook/springboot/service/posts/PostsService.java
+++ src/main/java/com/youngwook/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.youngwook.springboot.service.posts;
 
+import com.youngwook.springboot.domain.posts.Posts;
 import com.youngwook.springboot.domain.posts.PostsRepository;
+import com.youngwook.springboot.web.dto.PostsResponseDto;
 import com.youngwook.springboot.web.dto.PostsSaveRequestDto;
+import com.youngwook.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,23 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+
     }
 }
